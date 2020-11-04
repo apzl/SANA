6a9e9406d29800c4fd00c3e76b246a6649f20bd9
apzl <apsalshbk550@gmail.com>
2020-11-04 09:33:42 +0530

    template

--- sana/settings.py
+++ sana/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static')
-]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
