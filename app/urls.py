12133dc5a1fa2a9782fc0a2af3da45130d2be87c
basilbenil <beniljosenellad@gmail.com>
2020-11-04 17:20:18 +0530

    first commit

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ router.register(r'app', views.AudioViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls',namespace='rest_framework')),
-	path('upload/',views.upload_audio,name='upload_audio'),
+	path('home/',views.upload_audio,name='upload_audio'),
     path('list/',views.list_audio,name='list_audio'),
     path('predict/',views.predict_audio,name='predict_audio'),
 
